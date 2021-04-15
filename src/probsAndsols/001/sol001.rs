45a6c4391823604e99c80bcd76ef5074fa079ee1
Thandile Nododile <nododilereply@gmail.com>
2021-04-15 23:52:07 +0200

    unfinished process method

--- src/probsAndsols/001/sol001.rs
+++ src/probsAndsols/001/sol001.rs
@@ -8,12 +8,26 @@ fn main() {
         .expect("Failed to read the line");
 
     let words_count_int: u32 = words_count.trim().parse().expect("Please type a number!");
+    let mut words_array = Vec::new();
     let mut counter: u32 = 1;
     while counter <= words_count_int {
         let mut user_string = String::new();
         io::stdin()
             .read_line(&mut user_string)
             .expect("Failed to read the line");
+        words_array.push(user_string);
         counter += 1;
+    }
+
+    for word in words_array{
+        process_word(word);
+    }
+}
+
+fn process_word(word:String){
+    if word.len() > 10 {
+        print!("something happens here!");
+    }else{
+        print!("{}",word);
     }
 }
